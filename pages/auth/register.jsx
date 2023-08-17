2b78c07608464392d9de67901cb23037d0e8a187
gajevski <mikolajxgajewski@gmail.com>
2023-08-17 15:53:07 +0200

    Add register texts

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -5,11 +5,9 @@ export default function Register() {
     return (
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-lg text-center">
-                <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
-
+            <h1 className="text-2xl font-bold sm:text-3xl">Periodyk 🧛‍♀️</h1>
                 <p className="mt-4 text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
-                    eaque error neque ipsa culpa autem, at itaque nostrum!
+                    Stwórz nowe konto
                 </p>
             </div>
 
