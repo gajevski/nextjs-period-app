97fd83bce6658560459d26e0058f9ceab7ba51cd
gajevski <mikolajxgajewski@gmail.com>
2023-08-20 23:22:38 +0200

    Refactor add period backend

--- pages/api/periods/add-period.js
+++ pages/api/periods/add-period.js
@@ -1,20 +1,22 @@
-const periods = [
-    { id: 0, startDate: "2023-06-10", description: "Period one" },
-    { id: 1, startDate: "2023-07-15", description: "Period two" },
-    { id: 2, startDate: "2023-08-21", description: "Period three" },
-];
+import { periodsData } from "../../../shared/periods";
 
 function generateRandomId() {
     return Math.floor(Math.random() * 1000);
 }
 
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const periodData = req.body;
-        periodData.id = generateRandomId();
-        periods.push(periodData);
-        res.status(200).json({ periods });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
+  const { startDate, description } = req.body;
+
+  // Generate a unique ID for the new period
+  const newPeriodId = generateRandomId();
+
+  const newPeriod = {
+    id: newPeriodId,
+    startDate,
+    description,
+  };
+
+  periodsData.push(newPeriod);
+
+  res.status(201).json({ message: "Period added successfully.", period: newPeriod });
 }
