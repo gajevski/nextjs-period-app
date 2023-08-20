3a914ce8b843fec4117afd5a088862f2951b566d
gajevski <mikolajxgajewski@gmail.com>
2023-08-20 23:23:12 +0200

    Add dummy periods data

---
+++ shared/periods.js
@@ -0,0 +1,5 @@
+export let periodsData = [
+    { id: 0, startDate: "2023-06-10", description: "Period one" },
+    { id: 1, startDate: "2023-07-15", description: "Period two" },
+    { id: 2, startDate: "2023-08-21", description: "Period three" },
+];
