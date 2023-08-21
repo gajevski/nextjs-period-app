8d2d5cf458c9a77018f4172754786cfe12c0230c
gajevski <mikolajxgajewski@gmail.com>
2023-08-21 09:51:50 +0200

    Use Valentine theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,6 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [require("@tailwindcss/typography"), require('daisyui')],
   daisyui: {
-    themes: ["fantasy"],
+    themes: ["valentine"],
   },
 };
