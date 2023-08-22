d61f0b5ada87fd1fcd4736141381ec00ec874073
gajevski <mikolajxgajewski@gmail.com>
2023-08-22 17:46:09 +0200

    Add loader to the current periods

--- pages/periods/current.jsx
+++ pages/periods/current.jsx
@@ -2,8 +2,18 @@ import Link from "next/link";
 import React, { useState, useEffect } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 
+const SkeletonPeriod = () => (
+  <div className="flex items-center justify-center m-4 h-32 card bg-base-300 rounded-box animate-pulse">
+    <article className="prose lg:prose-xl">
+    <span className="loading loading-ring loading-sm"></span>
+    </article>
+  </div>
+);
+
 export default function CurrentPeriod() {
   const [periods, setPeriods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const options = {
     weekday: 'long',
     year: 'numeric',
@@ -14,8 +24,14 @@ export default function CurrentPeriod() {
   useEffect(() => {
     fetch('/api/periods/next-period')
       .then(response => response.json())
-      .then(data => setPeriods(data))
-      .catch(error => console.error('Error fetching periods:', error));
+      .then(data => {
+        setPeriods(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching periods:', error);
+        setLoading(false);
+      });
   }, []);
 
   const lastPeriod = periods.lastPeriod;
@@ -30,23 +46,33 @@ export default function CurrentPeriod() {
         <h1 className="px-5 py-2">Najbliższy okres</h1>
       </article>
       <div className="flex flex-col w-full lg:flex-row">
-        <div className="grid m-4 flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-        <article className="prose lg:prose-xl">
-          <h3 className="px-5">Ostatni okres wypadł:</h3>
-          {lastStartDate && (
-            <p className="px-5">{lastStartDate.toLocaleDateString(undefined, options)}</p>
-          )}
-          </article>
-        </div>
-        <div className="divider lg:divider-horizontal">🩸</div>
-        <div className="grid m-4 flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-        <article className="prose lg:prose-xl">
-          <h3 className="px-5">Następny okres wypada:</h3>
-          {nextStartDate && (
-            <p className="px-5">{nextStartDate.toLocaleDateString(undefined, options)}</p>
-          )}
-          </article>
-        </div>
+        {loading ? (
+          <>
+            <SkeletonPeriod />
+            <div className="divider lg:divider-horizontal">🩸</div>
+            <SkeletonPeriod />
+          </>
+        ) : (
+          <>
+            <div className="grid m-4 flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+              <article className="prose lg:prose-xl">
+                <h3 className="px-5">Ostatni okres wypadł:</h3>
+                <p className="px-5">
+                  {lastStartDate && lastStartDate.toLocaleDateString(undefined, options)}
+                </p>
+              </article>
+            </div>
+            <div className="divider lg:divider-horizontal">🩸</div>
+            <div className="grid m-4 flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+              <article className="prose lg:prose-xl">
+                <h3 className="px-5">Następny okres wypada:</h3>
+                <p className="px-5">
+                  {nextStartDate && nextStartDate.toLocaleDateString(undefined, options)}
+                </p>
+              </article>
+            </div>
+          </>
+        )}
       </div>
       <div className="btm-nav">
         <button className="text-error active">
